448267f4b6066f4bd6a76bbbb3efea96946e683a
Michael Hueschen <michaelhueschen@gmail.com>
2012-12-23 00:23:20 -0800

    Active/Inactive/All views and landing page for album adding

--- static/app/js/controllers.js
+++ static/app/js/controllers.js
@@ -6,6 +6,20 @@
 function WelcomeCtrl() {}
 WelcomeCtrl.$inject = [];
 
-function HoldersCtrl($scope, $routeParams, Holders) {
+function HoldersCtrl($scope, $routeParams, $location, Holders) {
     var holders = $scope.holders = Holders.query({userId: $routeParams.userId});
+
+    $scope.$watch( 'routeParams.display', function( type ) {
+        $scope.display_type = ($routeParams.display == 'inactive') ?
+          'inactive' : ($routeParams.display == 'all') ?
+            'all' : 'active';
+        $scope.route_path = $location.path();
+        $scope.activeFilter = ($routeParams.display == 'inactive') ?
+          { active: false } : ($routeParams.display == 'all') ?
+            null : { active: true };
+    });
+}
+
+function AddCtrl($scope) {
+    
 }
