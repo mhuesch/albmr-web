3a6d1446664a382df197577fefdb07a4cb7d246c
Michael Hueschen <michaelhueschen@gmail.com>
2012-12-24 16:37:40 -0800

    Implemented deletion of an AlbumHolding.
    Switched to use $http instead of lame $resource

--- static/app/js/controllers.js
+++ static/app/js/controllers.js
@@ -6,8 +6,13 @@
 function WelcomeCtrl() {}
 WelcomeCtrl.$inject = [];
 
-function HoldersCtrl($scope, $routeParams, $location, Holders) {
-    var holders = $scope.holders = Holders.query({userId: $routeParams.userId});
+function HoldersCtrl($scope, $routeParams, $location, Holders, AlbumHolding) {
+    var holders = $scope.holders = null;
+
+    var holdersPromise = Holders.query($routeParams.userId);
+    holdersPromise.then(function(hs) {
+        holders = $scope.holders = hs;
+    });
 
     $scope.holder_expanded = null;
 
@@ -30,10 +35,21 @@ function HoldersCtrl($scope, $routeParams, $location, Holders) {
     };
 
     $scope.removeHolder = function ( holder ) {
+        var answer = confirm("Are you sure you want to remove this album?");
+        if (answer) {
+            var albumHoldingPromise = AlbumHolding.delete(holder.id);
+            albumHoldingPromise.then(function(status) {
+                if (status == 204) {
+                    $scope.spliceHolder(holder);
+                } else {
+                    alert("Delete failed");
+                }
+            });
+        }
+    };
+
+    $scope.spliceHolder = function ( holder ) {
         holders.splice(holders.indexOf(holder), 1);
-        console.log("Holder removed");
-        console.dir(holder);
-        // Call a function to send a delete to the server for this holder
     };
 }
 
--- static/app/js/services.js
+++ static/app/js/services.js
@@ -2,9 +2,49 @@
 
 /* Services */
 
+
+// Get csrf token
+var csrftoken = $.cookie('csrftoken');
+
 angular.module('albmr.services', ['ngResource']).
-    factory('Holders', function($resource){
-  return $resource('api/v1/user/:userId/holders', {}, {
-    query: {method:'GET', isArray:true}
-  });
-});
+    factory('Holders', function($http) {
+        // Holders is a class for retrieving data from the server
+        var Holders = function(data) {
+            angular.extend(this, data);
+        }
+
+        Holders.query = function(id) {
+            return $http.get('api/v1/user/' + id + '/holders/').then(function(response) {
+                return response.data;
+            });
+        };
+
+        return Holders;
+    }).
+    factory('AlbumHolding', function($http) {
+        var AlbumHolding = function(data) {
+            angular.extend(this, data);
+        }
+
+        AlbumHolding.put = function(ah) {
+            return $http.put('api/v1/albumholding/' + ah.id + '/', ah, {headers: {'X-CSRFToken' : csrftoken }}).then(function(response) {
+                return new AlbumHolding(response.data);
+            });
+        };
+
+        AlbumHolding.delete = function(id) {
+            return $http.delete('api/v1/albumholding/' + id + '/', {headers: {'X-CSRFToken' : csrftoken }}).then(function(response) {
+                return response.status;
+            });
+        };
+
+        return AlbumHolding;
+
+/*
+        return $resource('api/v1/albumholding/:ahId/', {}, {
+            update: {method:'PUT', headers: {'X-CSRFToken' : csrftoken }},
+            delete: {method:'DELETE', headers: {'X-CSRFToken' : csrftoken }}
+        });
+*/
+    });
+
